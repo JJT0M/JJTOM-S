ed8a29e44762d1d53b2b63ffd4ec252ef7886b94
JJ.TOM <159321083+JJT0M@users.noreply.github.com>
2024-02-09 16:07:20 +0800

    Add files via upload

---
+++ SHUJULEIXING.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+
+{
+
+char ch = 'a';
+short age = 20;
+int weight = 65;
+long salary = 12000;
+long long population = 455421;
+float pi =3.231;
+double e = 2.736464;
+
+printf("%d\n",sizeof(char));
+printf("%d\n",sizeof(short));
+printf("%d\n",sizeof(int));
+printf("%d\n",sizeof(long));
+printf("%d\n",sizeof(long long));
+printf("%d\n",sizeof(float));
+printf("%d\n",sizeof(double));
+
+return 0;
+
+
+}
